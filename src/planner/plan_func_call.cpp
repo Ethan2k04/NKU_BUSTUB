4e6b545e75f15515cf8f16fa5c4dd5953031605c
Tengjiao Yin <60817795+Ethan2k04@users.noreply.github.com>
2024-03-30 14:12:47 +0800

    Project 0 Task #2-SQL String Functions

--- src/planner/plan_func_call.cpp
+++ src/planner/plan_func_call.cpp
@@ -29,7 +29,19 @@ auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<A
   // 1. check if the parsed function name is "lower" or "upper".
   // 2. verify the number of args (should be 1), refer to the test cases for when you should throw an `Exception`.
   // 3. return a `StringExpression` std::shared_ptr.
-  throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
+  // throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
+  StringExpressionType expr_type;
+  if (func_name == "lower" || func_name == "upper") {
+    if (args.size() != 1) {
+      throw Exception(fmt::format("func call {} requires 1 argument", func_name));
+    }
+    if (func_name == "lower") {
+      expr_type = StringExpressionType::Lower;
+    } else if (func_name == "upper") {
+      expr_type = StringExpressionType::Upper;
+    }
+  }
+  return std::make_shared<StringExpression>(args[0], expr_type);
 }
 
 }  // namespace bustub
