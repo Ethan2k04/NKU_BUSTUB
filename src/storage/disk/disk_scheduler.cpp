1e1115c674c0e37db5137bf5ac7885dbbe21992a
Tengjiao Yin <60817795+Ethan2k04@users.noreply.github.com>
2024-04-01 02:38:08 +0800

    Project 1 Task #2 - Disk Scheduler

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -18,9 +18,9 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
+  // throw NotImplementedException(
+  //    "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+  //    "throw exception line in `disk_scheduler.cpp`.");
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
@@ -34,8 +34,28 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+// Custom function to process request
+void DiskScheduler::ProcessDiskRequest(DiskRequest r) {
+  // Process write request
+  if (r.is_write_) {
+    disk_manager_->WritePage(r.page_id_, r.data_);
+  } else {
+    // Process read request
+    disk_manager_->ReadPage(r.page_id_, r.data_);
+  }
+  r.callback_.set_value(true);
+}
 
-void DiskScheduler::StartWorkerThread() {}
+// Simply add the request to the queue
+void DiskScheduler::Schedule(DiskRequest r) { request_queue_.Put(std::move(r)); }
 
+// Maintain a worker thread in the background
+void DiskScheduler::StartWorkerThread() {
+  std::optional<DiskRequest> r;
+  while ((r = request_queue_.Get()) != std::nullopt) {
+    // Fork a thread to process disk request
+    std::thread t = std::thread(&DiskScheduler::ProcessDiskRequest, this, std::move(r.value()));
+    t.join();
+  }
+}
 }  // namespace bustub
